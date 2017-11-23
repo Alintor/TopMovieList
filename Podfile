2911a889b6f44ca1ca90907bf8c4266a1f980a3e
Alexandr Ovchinnikov <Alintorius@gmail.com>
2017-11-23 19:29:16 +0700

    create project and install pods

---
+++ Podfile
@@ -0,0 +1,8 @@
+
+target 'TopMovieList' do
+  use_frameworks!
+
+  pod 'SwiftyJSON'
+  pod 'Kingfisher'
+
+end
---
+++ Pods/Target Support Files/Pods-TopMovieList/Pods-TopMovieList-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TopMovieList : NSObject
+@end
+@implementation PodsDummy_Pods_TopMovieList
+@end
